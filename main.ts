654adeaa5f6fa87638db7ce118486b3083d27975
Garrett Serack <garretts@microsoft.com>
2018-02-02 16:13:08 -0800

    compiles. that's it.

--- main.ts
+++ main.ts
@@ -1,2 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
-console.log("hi");
+import { AutoRestExtension, } from "@microsoft.azure/autorest-extension-base";
+import { process as normalizer } from "./normalizer/main";
+
+async function main() {
+  const pluginHost = new AutoRestExtension();
+
+  pluginHost.Add("normalizer", normalizer);
+
+  await pluginHost.Run();
+}
+
+main();
