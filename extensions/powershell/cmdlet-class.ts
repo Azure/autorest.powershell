fa56f80cf9f3163e7b1d6f4182f0fb4d67d05cf6
Nelson Daniel Troncoso <nd.ta@hotmail.com>
2019-04-04 15:01:24 -0700

    updated gen-attribute for cmdlet-class

--- extensions/powershell/cmdlet-class.ts
+++ extensions/powershell/cmdlet-class.ts
@@ -809,7 +809,7 @@ export class CmdletClass extends Class {
     }
 
     this.add(new Attribute(DescriptionAttribute, { parameters: [new StringExpression(this.description)] }))
-    this.add(new Attribute(GeneratedAttribute));
+    this.add(new Attribute(GeneratedAttribute, { parameters: [`"AutoRest"`, `"${this.state.project.autorestVersion}"`] }));
     if (operation.extensions && operation.extensions['x-ms-metadata'] && operation.extensions['x-ms-metadata'].profiles) {
       const profileNames = Object.keys(operation.extensions['x-ms-metadata'].profiles);
       // wrap profile names
