567e07ca1328b9b270c72302431b3cf25d647bf2
Garrett Serack <garretts@microsoft.com>
2019-08-08 14:24:41 -0700

    better filter

--- extensions/remodeler/plugin-tweak-model-azure.ts
+++ extensions/remodeler/plugin-tweak-model-azure.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 
-import { codemodel, JsonType, processCodeModel, ModelState, getAllProperties } from '@microsoft.azure/autorest.codemodel-v3';
+import { codemodel, JsonType, processCodeModel, ModelState, getAllProperties, HttpMethod, schema } from '@microsoft.azure/autorest.codemodel-v3';
 import { keys, length, values } from '@microsoft.azure/codegen';
 
 import { Channel, Host } from '@microsoft.azure/autorest-extension-base';
@@ -150,10 +150,22 @@ async function tweakModel(state: State): Promise<codemodel.Model> {
 
   // when make-sub-resources-byreference is specified, mark models with a writable id as byref.
   if (state.getValue('azure', false) && state.getValue('make-sub-resources-byreference')) {
-    for (const each of values(model.schemas)) {
-      const props = getAllProperties(each);
-      if (props.find(prop => prop.serializedName === 'id' && !prop.details.default.readOnly)) {
-        each.details.default.byReference = true;
+
+    for (const schema of values(model.schemas)) {
+      // find schemas that have an 'id' and are not readonly
+      if (values(getAllProperties(schema)).linq.any(prop => prop.serializedName === 'id' && !prop.details.default.readOnly)) {
+
+        // look thru the operations, and the PUT methods 
+        for (const op of values(model.http.operations).linq.where(o => o.method === HttpMethod.Put)) {
+
+          // see if any of the responses have the same schema as we are looking for 
+          if (values(op.responses).linq.any(rsps => values(rsps).linq.any(resp => !!resp.schema && resp.schema.details.default.uid === schema.details.default.uid))) {
+
+            // tell it not to inline that 
+            schema.details.default.byReference = true;
+          }
+
+        }
       }
     }
   }
