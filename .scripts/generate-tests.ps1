d0ffcf438b659e22511734c1e6419ab6323d9d27
Garrett Serack <garretts@microsoft.com>
2019-10-03 10:42:02 -0700

    tweak test generation

--- .scripts/generate-tests.ps1
+++ .scripts/generate-tests.ps1
@@ -2,7 +2,17 @@
 param (
     [Parameter()]
     [string[]]
-    $swaggers
+    $swaggers,
+
+    [Parameter()]
+    [string]
+    $outputRoot = $null,
+
+    [Parameter()]
+    [switch]
+    $useLocalAutorest
+
+
 )
 # load test names
 . $PSScriptRoot/tests.ps1
@@ -12,24 +22,33 @@ if( $swaggers -ne $null) {
   $inputs = $inputs |% { if( $swaggers.indexOf( $_ ) -gt -1 )  { return $_ } }
 }
 
-
 # generates the AutoRest tests into separate modules
 
 $root = ( resolve-path "$PSScriptRoot/..").Path
-$outputRoot = ( resolve-path "$root/tests").Path
+
+if( ($outputRoot -eq $null ) -or ($outputRoot -eq ''))  {
+  $outputRoot = ( resolve-path "$root/tests").Path
+}
+$null = mkdir $outputRoot -force 
 
 cd $root
 
 # start @autorest/test-server
 ./powershell/node_modules/.bin/start-autorest-testserver
 
-start-sleep 3
+start-sleep 5
 
 # source location for swagger files
 $swaggerRoot = "http://localhost:3000/swagger/"
  
 # AutoRest Choice
-$autorest = (get-command autorest-beta).Source 
+if( $useLocalAutorest) {
+  $autorest='node' 
+  $autorestArgs = @(  (resolve-path $root/../autorest), "--version:$(resolve-path $root/../autorest)" )
+} else {
+  $autorest = (get-command autorest-beta).Source 
+}
+
 
 # use local generator
 $powershellGenerator="--use:$(resolve-path $root/powershell)"
@@ -49,10 +68,11 @@ function run-autorest($src) {
   
   $outputFolder = "$outputRoot/$name"
   
-  $txt = "$autorest $powershellGenerator $remodeler --input-file:$swaggerRoot$src --output-folder:$outputFolder --clear-output-folder --title:$name $args" 
+  $txt = "$autorest $autorestArgs $powershellGenerator $remodeler --input-file:$swaggerRoot$src --output-folder:$outputFolder --clear-output-folder --title:$name $args" 
   write-host -fore GREEN "`n--------------------------------------------------------`nGenerating [$name]`n--------------------------------------------------------`n"
   
-  & $autorest $powershellGenerator $remodeler "--input-file:$swaggerRoot$src" "--output-folder:$outputFolder" "--clear-output-folder" "--title:$name"  $args
+  
+  & $autorest $autorestArgs $powershellGenerator $remodeler "--input-file:$swaggerRoot$src" "--output-folder:$outputFolder" "--clear-output-folder" "--title:$name"  $args
   $rc = $LastExitCode
   if( $rc -gt 0 ) {
     write-host -fore RED "`n--------------------------------------------------------`nFAILED GENERATION [$name]`n--------------------------------------------------------`n"
@@ -75,6 +95,12 @@ $n = 0;
 
 $inputs |% {
  if( $n -le 99 ) {
+
+  $j = get-process java  -ea 0 
+  if( $j -eq $null ) {
+    exit 1;
+  }
+
     run-autorest $_
   }
   $n = $n + 1
