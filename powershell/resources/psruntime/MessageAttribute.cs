d531c167e21c47b7176eb6cf1dbfe3832652958d
derek <xidi@microsoft.com>
2021-08-02 11:59:37 +0800

    Change date format for breaking change

--- powershell/resources/psruntime/MessageAttribute.cs
+++ powershell/resources/psruntime/MessageAttribute.cs
@@ -83,7 +83,7 @@ namespace Microsoft.Message.ClientRuntime
 
             if (ChangeInEfectByDateSet)
             {
-                writeOutput(string.Format(Resources.BreakingChangesAttributesInEffectByDateMessage, this.ChangeInEfectByDate));
+                writeOutput(string.Format(Resources.BreakingChangesAttributesInEffectByDateMessage, this.ChangeInEfectByDate.ToString("d")));
             }
 
             if (DeprecateByVersionSet)
