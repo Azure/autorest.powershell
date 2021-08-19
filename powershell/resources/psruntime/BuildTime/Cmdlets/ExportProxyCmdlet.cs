64394f1f139ac3a4446a25a4c93ee05c097b7318
dolauli <xidi@microsoft.com>
2021-08-19 11:01:16 +0800

    Remove duplicated license from ProxyCmdletDefinitions, issue#814

--- powershell/resources/psruntime/BuildTime/Cmdlets/ExportProxyCmdlet.cs
+++ powershell/resources/psruntime/BuildTime/Cmdlets/ExportProxyCmdlet.cs
@@ -69,20 +69,8 @@ namespace Microsoft.Rest.ClientRuntime.PowerShell
                     Path.Combine(vg.Key.IsInternal ? InternalFolder : ExportsFolder, pg.ProfileFolder), pg.ProfileName, isInternal: vg.Key.IsInternal)))
                 .ToArray();
 
-            foreach (var variantGroup in variantGroups)
-            {
-                var parameterGroups = variantGroup.ParameterGroups.ToList();
-                var isValidProfile = !String.IsNullOrEmpty(variantGroup.ProfileName) && variantGroup.ProfileName != NoProfiles;
-                var examplesFolder = isValidProfile ? Path.Combine(ExamplesFolder, variantGroup.ProfileName) : ExamplesFolder;
-                var markdownInfo = new MarkdownHelpInfo(variantGroup, examplesFolder);
-                List<PsHelpExampleInfo> examples = new List<PsHelpExampleInfo>();
-                foreach (var it in markdownInfo.Examples)
-                {
-                    examples.Add(it);
-                }
-                variantGroup.HelpInfo.Examples = examples.ToArray();
-                var sb = new StringBuilder();
-                sb.Append(@"
+            var license = new StringBuilder();
+            license.Append(@"
 # ----------------------------------------------------------------------------------
 #
 # Copyright Microsoft Corporation
@@ -97,6 +85,20 @@ namespace Microsoft.Rest.ClientRuntime.PowerShell
 # limitations under the License.
 # ----------------------------------------------------------------------------------
 ");
+            HashSet<string> LicenseSet = new HashSet<string>();
+            foreach (var variantGroup in variantGroups)
+            {
+                var parameterGroups = variantGroup.ParameterGroups.ToList();
+                var isValidProfile = !String.IsNullOrEmpty(variantGroup.ProfileName) && variantGroup.ProfileName != NoProfiles;
+                var examplesFolder = isValidProfile ? Path.Combine(ExamplesFolder, variantGroup.ProfileName) : ExamplesFolder;
+                var markdownInfo = new MarkdownHelpInfo(variantGroup, examplesFolder);
+                List<PsHelpExampleInfo> examples = new List<PsHelpExampleInfo>();
+                foreach (var it in markdownInfo.Examples)
+                {
+                    examples.Add(it);
+                }
+                variantGroup.HelpInfo.Examples = examples.ToArray();
+                var sb = new StringBuilder();
                 sb.Append($"{Environment.NewLine}");
                 sb.Append(variantGroup.ToHelpCommentOutput());
                 sb.Append($"function {variantGroup.CmdletName} {{{Environment.NewLine}");
@@ -134,8 +136,13 @@ namespace Microsoft.Rest.ClientRuntime.PowerShell
                 sb.Append($"}}{Environment.NewLine}");
 
                 Directory.CreateDirectory(variantGroup.OutputFolder);
-                File.WriteAllText(variantGroup.FilePath, sb.ToString());
-
+                File.WriteAllText(variantGroup.FilePath, license.ToString());
+                File.AppendAllText(variantGroup.FilePath, sb.ToString());
+                if (!LicenseSet.Contains(Path.Combine(variantGroup.OutputFolder, "ProxyCmdletDefinitions.ps1"))) {
+                    // only add license in the header
+                    File.AppendAllText(Path.Combine(variantGroup.OutputFolder, "ProxyCmdletDefinitions.ps1"), license.ToString());
+                    LicenseSet.Add(Path.Combine(variantGroup.OutputFolder, "ProxyCmdletDefinitions.ps1"));
+                }
                 File.AppendAllText(Path.Combine(variantGroup.OutputFolder, "ProxyCmdletDefinitions.ps1"), sb.ToString());
             }
 
