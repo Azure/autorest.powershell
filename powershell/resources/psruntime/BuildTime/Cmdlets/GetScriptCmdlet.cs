c721de07a68b07a129cd1f3da924790ef307bd56
Beisi Zhou <zazbs@qq.com>
2021-09-27 14:10:11 +0800

    Output error message in GetScriptCmdlet.cs

--- powershell/resources/psruntime/BuildTime/Cmdlets/GetScriptCmdlet.cs
+++ powershell/resources/psruntime/BuildTime/Cmdlets/GetScriptCmdlet.cs
@@ -45,7 +45,8 @@ namespace Microsoft.Rest.ClientRuntime.PowerShell
       }
       catch (System.Exception ee)
       {
-        System.Console.WriteLine($"${ee.GetType().Name}/{ee.StackTrace}");
+        System.Console.Error.WriteLine($"{ee.GetType().Name}: {ee.Message}");
+        System.Console.Error.WriteLine(ee.StackTrace);
         throw ee;
       }
     }
