1b413e492b152fd3ea397a712804a55fc304dd75
Timothy Wamalwa <twamalwa@microsoft.com>
2022-06-16 14:25:09 +0300

    Escape angle brackets for placeholders

--- powershell/resources/psruntime/BuildTime/Models/PsProxyOutputs.cs
+++ powershell/resources/psruntime/BuildTime/Models/PsProxyOutputs.cs
@@ -491,7 +491,7 @@ namespace Microsoft.Rest.ClientRuntime.PowerShell
             var leftOptional = !IsMandatory ? "[" : String.Empty;
             var leftPositional = Position != null ? "[" : String.Empty;
             var rightPositional = Position != null ? "]" : String.Empty;
-            var type = ParameterType != typeof(SwitchParameter) ? $" <{ParameterType.ToSyntaxTypeName()}>" : String.Empty;
+            var type = ParameterType != typeof(SwitchParameter) ? $" \\<{ParameterType.ToSyntaxTypeName()}\\>" : String.Empty;
             var rightOptional = !IsMandatory ? "]" : String.Empty;
             var space = IncludeSpace ? " " : String.Empty;
             var dash = IncludeDash ? "-" : String.Empty;
