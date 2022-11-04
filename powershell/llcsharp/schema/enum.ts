02f0720ac065c0fc1156a7d7fa9cb4684c1a61e7
Peter Ombwa <peombwa@microsoft.com>
2022-11-03 19:41:35 -0700

    Deserialize enum responses. (#1010)

--- powershell/llcsharp/schema/enum.ts
+++ powershell/llcsharp/schema/enum.ts
@@ -6,7 +6,8 @@
 import { Schema } from '../code-model';
 import { Schema as NewSchema } from '@azure-tools/codemodel';
 import { String } from './string';
-import { dotnet, toExpression } from '@azure-tools/codegen-csharp';
+import { dotnet, Expression, ExpressionOrLiteral, toExpression } from '@azure-tools/codegen-csharp';
+import { KnownMediaType } from '@azure-tools/codemodel-v3';
 
 export class EnumImplementation extends String {
   public isXmlAttribute = false;
@@ -29,4 +30,13 @@ export class EnumImplementation extends String {
 
 
   get declaration(): string { return `${this.schema.language.csharp?.namespace}.${this.schema.language.csharp?.name}${this.isRequired ? '' : '?'}`; }
+
+    /** emits an expression to deserialize content from a content/response */
+  deserializeFromResponse(mediaType: KnownMediaType, content: ExpressionOrLiteral, defaultValue: Expression): Expression | undefined {
+    switch (mediaType) {
+      case KnownMediaType.Json:
+        return toExpression(`${content}.Content.ReadAsStringAsync().ContinueWith( body => ${this.deserializeFromString(mediaType, 'body.Result', defaultValue)})`);
+    }
+    return toExpression(`null /* deserializeFromResponse doesn't support '${mediaType}' ${__filename}*/`);
+  }
 }
