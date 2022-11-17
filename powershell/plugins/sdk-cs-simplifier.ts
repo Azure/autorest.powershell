0573874d3c4cff264f0322509038d523e218c3af
VeryEarly <yabhu@microsoft.com>
2022-11-17 22:57:52 +0800

    fix comparator

--- powershell/plugins/sdk-cs-simplifier.ts
+++ powershell/plugins/sdk-cs-simplifier.ts
@@ -13,29 +13,48 @@ export async function simplifierPlugin(service: Host) {
   const trimTasks = files.map(async file => {
     let namespacesToAdd: Set<string> = new Set<string>();
     const trimmed = await trimNamespace(await service.ReadFile(file), namespacesToAdd);
-    const endOfLastUsing = findLastUsing(trimmed);
-    service.WriteFile(file, addUsings(trimmed, endOfLastUsing, namespacesToAdd), undefined, 'source-file-csharp');
+    const usings = findLastUsing(trimmed);
+    service.WriteFile(file, addUsings(trimmed, usings[0], usings[1], sortNamespaces([...[...namespacesToAdd].map(namespace => `\n    using ${namespace};`), ...usings[2]])), undefined, 'source-file-csharp');
   });
   await Promise.all(trimTasks);
 }
 
-function addUsings(content: string, start: number, namespaceToAdd: Set<string>): string {
-  let namespaces: string = [...namespaceToAdd].map(namespace => `\n    using ${namespace};`).join('');
-  return content.substring(0, start).concat(namespaces).concat(content.substring(start));
+function addUsings(content: string, start: number, end: number, namespacesToAdd: Array<string>): string {
+  let namespaces: string = namespacesToAdd.join('');
+  return content.substring(0, start).concat(namespaces).concat(content.substring(end));
 }
 
-function findLastUsing(content: string): number {
+function findLastUsing(content: string): [number, number, Array<string>] {
+  let index = 0;
   let start = 0;
   let end = 0;
-  while (end != -1) {
-    let segment: string = content.substring(start);
-    end = segment.search(usingRegex);
-    if (end != -1) {
-      end = segment.indexOf(';', end) + 1;
-      start += end;
+  let existingUsings: Array<string> = new Array<string>();
+  while (start != -1) {
+    let segment: string = content.substring(index);
+    start = segment.search(usingRegex);
+    if (start != -1) {
+      end = segment.indexOf(';', start) + 1;
+      existingUsings.push(segment.substring(start, end));
+      index += end;
     }
   }
-  return start;
+  return [content.search(usingRegex), index, existingUsings];
+}
+
+function sortNamespaces(namespaces: Array<string>): Array<string> {
+  return namespaces.sort((a, b) => {
+    const arrA: Array<string> = a.split(';')[0].split('.');
+    const arrB: Array<string> = b.split(';')[0].split('.');
+    const length: number = arrA.length < arrB.length ? arrA.length : arrB.length;
+    for (let i = 0; i < length; i++) {
+      if (arrA[i] < arrB[i]) {
+        return -1;
+      } else if (arrA[i] > arrB[i]) {
+        return 1;
+      }
+    }
+    return arrA.length - arrB.length;
+  });
 }
 
 async function trimNamespace(content: string, namespacesToAdd: Set<string>): Promise<string> {
@@ -79,7 +98,7 @@ function trim(content: string, start: number, end: number): string {
 
 const predifinedNamespaces: Set<string> = new Set<string>(['Newtonsoft.Json', 'Newtonsoft.Json.Converters', 'System.Collections', 'System.Collections.Generic', 'System.Net', 'System.Net.Http', 'System.Threading', 'System.Threading.Tasks', 'Microsoft.Rest.Serialization', 'System.IO', 'System.Runtime', 'System.Runtime.Serialization']);
 const roots: Map<string, TrieNode> = initTrie(predifinedNamespaces);
-const usingRegex: string = '\n    using .*;';
+const usingRegex = '\n    using .*;';
 
 function initTrie(predifinedNamespaces: Set<string>): Map<string, TrieNode> {
   let namespaceRoots: Map<string, TrieNode> = new Map<string, TrieNode>();
