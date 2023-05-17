ccff0e4a8c42af74f5847d3781e5a2b959719d9b
Vincent Dai <23257217+vidai-msft@users.noreply.github.com>
2023-05-16 18:46:33 -0700

    resolve conflict with 96e95ea34596866e44e8a831e8818690bb6bf6ba

--- powershell/cmdlets/class.ts
+++ powershell/cmdlets/class.ts
@@ -1192,8 +1192,7 @@ export class CmdletClass extends Class {
           const props = NewGetAllPublicVirtualProperties(schema.language.csharp?.virtualProperties);
           const rType = $this.state.project.schemaDefinitionResolver.resolveTypeDeclaration(<NewSchema>schema, true, $this.state);
 
-          const awaitExpression = (length(props) === 1 && !apiCall.language.csharp?.pageable) ? `(await response).${props[0].name}` : '(await response)';
-          const result = new LocalVariable('result', dotnet.Var, { initializer: new LiteralExpression(awaitExpression) });
+          const result = new LocalVariable('result', dotnet.Var, { initializer: new LiteralExpression('(await response)') });
           yield `// (await response) // should be ${rType.declaration}`;
           yield result.declarationStatement;
 
@@ -1953,7 +1952,7 @@ export class CmdletClass extends Class {
         const props = NewGetAllProperties(schema);
 
         // does the target type just wrap a single output?
-        const resultSchema = length(props) !== 1 ? <NewSchema>schema : <NewSchema>props[0].schema;
+        const resultSchema = <NewSchema>schema;
 
         // make sure return type for boolean stays boolean!
         if (resultSchema.type === SchemaType.Boolean ||
@@ -2105,4 +2104,3 @@ export class CmdletClass extends Class {
     return false;
   }
 }
-
