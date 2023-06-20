7e35073777faa87f065309a16865cf4935ff63c2
Xiaogang <xidi@microsoft.com>
2023-06-20 16:21:00 +0800

    Update create-commands-v2.ts to fix a minor issue

--- powershell/plugins/create-commands-v2.ts
+++ powershell/plugins/create-commands-v2.ts
@@ -203,7 +203,7 @@ export /* @internal */ class Inferrer {
         const getOperation = getOperations.find(getOperation => getOperation.requests?.[0]?.protocol?.http?.path === putOperation?.requests?.[0]?.protocol?.http?.path);
         //parameter.protocal.http.in === 'body' probably only applies to open api 2.0
         const schema = putOperation?.requests?.[0]?.parameters?.find(p => p.protocol.http?.in === 'body')?.schema;
-        if (getOperation && schema && [...values(getOperation?.responses), ...values(getOperation?.exceptions)].filter(each => each.protocol?.http?.StatusCode !== 'default' && (<SchemaResponse>each).schema !== schema).length === 0) {
+        if (getOperation && schema && [...values(getOperation?.responses), ...values(getOperation?.exceptions)].filter(each => each.protocol?.http?.statusCodes[0] !== 'default' && (<SchemaResponse>each).schema !== schema).length === 0) {
           await this.addVariants(putOperation.parameters, putOperation, this.createCommandVariant('create', [operationGroup.$key], [], this.state.model), '', this.state, [getOperation], CommandOperationType.GetPut);
         }
       }
