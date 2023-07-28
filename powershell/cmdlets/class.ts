0e33ff0db3385fb1d92cbb94b40d390899869c09
Beisi Zhou <zazbs@qq.com>
2023-07-28 10:27:44 +0800

    Add missing semicolon if autoSwitchView is false

--- powershell/cmdlets/class.ts
+++ powershell/cmdlets/class.ts
@@ -609,7 +609,7 @@ export class CmdletClass extends Class {
         };
       }
     } else {
-      return `WriteObject(${valueName}, ${isEnumerable})`;
+      return `WriteObject(${valueName}, ${isEnumerable});`;
     }
   }
 
@@ -2103,7 +2103,7 @@ export class CmdletClass extends Class {
     if (operation.details.default.externalDocs) {
       this.add(new Attribute(ExternalDocsAttribute, {
         parameters: [`${new StringExpression(this.operation.details.default.externalDocs?.url ?? '')}`,
-          `${new StringExpression(this.operation.details.default.externalDocs?.description ?? '')}`]
+        `${new StringExpression(this.operation.details.default.externalDocs?.description ?? '')}`]
       }));
     }
 
