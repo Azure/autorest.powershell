38dd664c34945c03686bb5d8cfa80d2916e78e68
Yabo Hu <yabhu@microsoft.com>
2024-03-25 13:54:29 +0800

    support default subscriptionid with util function (#1323)
    * do not remove loadEnv.ps1 while regenerate/rebuild
    
    * add function get-localsubscription
    
    * rename function
    
    * adjust subscription id in export cmdlets
    
    * typo
    
    * remove environment variable
    
    * move get-subscriptionidtestsafe to utils
    
    * add template loadenv back
    
    * fix mockpipeline type
    
    * fix
    
    * add tenant back in parameter default
    
    * use subscription id safe only in playback mode

--- powershell/generators/psm1.ts
+++ powershell/generators/psm1.ts
@@ -156,4 +156,3 @@ ${getProfileExportScript(`Join-Path $PSScriptRoot '${project.exportsFolder}'`, p
   psm1.trim();
   project.state.writeFile(project.psm1, `${psm1}`, undefined, 'source-file-powershell');
 }
-
